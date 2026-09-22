58de81b196d54fe5b59f7575753e9cd9f6b78247
fediukolya <olha.fediuk.kb.2025@lpnu.ua>
2026-09-22 20:13:55 +0300

    Add subtract function

--- src/devops_lab1/lib.py
+++ src/devops_lab1/lib.py
@@ -2,7 +2,10 @@ def add(a: float, b: float) -> float:
     """Обчислює та повертає суму двох чисел."""
     return a + b
 
-
 def multiply(a: float, b: float) -> float:
     """Обчислює та повертає добуток двох чисел."""
     return a * b
+
+def subtract(a: float, b: float) -> float:
+    """Обчислює та повертає різницю двох чисел."""
+    return a - b
